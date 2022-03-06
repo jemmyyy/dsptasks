1232351dfe8e96deef9ff6246b55f462e78d44cb
Ahmed Gamal <amgamal200111@gmail.com>
2022-03-06 21:39:16 +0200

    Shaimaa GUI Final Commit

--- Task 1 - Signal Viewer/code.py
+++ Task 1 - Signal Viewer/code.py
@@ -4,7 +4,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.uic import  loadUiType
-
+from pyqtgraph import PlotWidget,plot
+from PyQt5 import QtCore
+import numpy as np
+import pyqtgraph as pg
 import sys
 import os 
 from os import path 
@@ -17,6 +20,8 @@ class MainApp(QMainWindow , FORM_CLASS):
         QMainWindow.__init__(self)
         self.setupUi(self)
         self.Handel_UI()
+        self.counter = 0
+        self.pen = pg.mkPen('r')
     def Handel_UI(self):
         ## self = QMainWindow
         self.setWindowTitle('Multi-Channel Signal Viewer')
@@ -25,12 +30,19 @@ class MainApp(QMainWindow , FORM_CLASS):
         self.Handel_Slider()
     def Handel_Buttons(self):
         self.BrowseButton_13.clicked.connect(self.Handel_Browse)
-
+        self.ShowButton_4.clicked.connect(self.Handel_Show)
+        self.HideButton_5.clicked.connect(self.Handel_Hide)
     def Handel_Slider(self):
          pass
 
     def Handel_GraphicsViews(self):
          pass
+
+    def Handel_Show(self):
+          self.pen = pg.mkPen('r')
+
+    def Handel_Hide(self):
+          self.pen = pg.mkPen('k')
 
     def Handel_Browse(self):
          open_place = QFileDialog.getOpenFileName(self , caption= "Browse", directory=".", filter="All Files (*.*)")
@@ -45,6 +57,25 @@ class MainApp(QMainWindow , FORM_CLASS):
              self.amplitude.append(float(line [0]))
              self.time.append(float(line [1])) 
          myfile.close() 
+         self.x = np.array(self.time)
+         self.y = np.array(self.amplitude)
+         self.timer()
+
+    def timer(self):
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(200)                       #In control of speed :)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+        
+    
+    def update_plot_data(self):
+        self.graphicsView.plot(self.x[0:self.counter],self.y[0:self.counter], pen=self.pen)
+        self.counter = self.counter + 100
+        self.graphicsView.clear()
+
+        self.graphicsView.plot(self.x[0:self.counter],self.y[0:self.counter], pen=self.pen)               #Plot every 100 x index with 100 y index
+        if self.counter < len(self.x):                                                        #To stop at the limit of the graph
+            self.graphicsView.setXRange( self.counter-200,self.counter)
 
 def main ():
     app = QApplication(sys.argv)
