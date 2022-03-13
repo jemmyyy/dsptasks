98993db85b4c752286383411856771151feffe24
Mohammed El Saeed <mohammed.elsaeed1@gmail.com>
2022-03-13 19:28:29 +0200

    export

--- Task 1 - Signal Viewer/code.py
+++ Task 1 - Signal Viewer/code.py
@@ -13,11 +13,25 @@ import os
 from os import path 
 from tkinter import *
 from tkinter import ttk
+from PyQt5 import QtCore, QtGui, QtWidgets,QtPrintSupport
+from PyPDF2.pdf import PdfFileReader
+from PyQt5.QtPrintSupport import QPrintDialog, QPrinter, QPrintPreviewDialog
+from PyQt5.Qt import QFileInfo
+import matplotlib.pyplot as plt
+from numpy.lib.function_base import append
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_pdf import PdfPages, _create_pdf_info_dict
+from PyPDF2 import PdfFileMerger, merger
+import fitz
 
 root = Tk()
 root.title('Codemy.com - Learn To Code!')
 
 FORM_CLASS,_= loadUiType(path.join(path.dirname(__file__),"main.ui"))
+
+
+
+  
 
 class MainApp(QMainWindow , FORM_CLASS):
     def __init__(self, parent=None):
@@ -33,7 +47,7 @@ class MainApp(QMainWindow , FORM_CLASS):
         self.First_Signal_Flag = 0
         self.Second_Signal_Flag = 0
         self.Third_Signal_Flag = 0
-        
+        self.speed=1000
         self.isRunning=True
 
         self.Hide_First_Signal_Flag = 0
@@ -55,12 +69,12 @@ class MainApp(QMainWindow , FORM_CLASS):
         self.BrowseButton_13.clicked.connect(self.Handel_Browse)
         self.ShowcomboBox.currentTextChanged.connect(self.Handel_Show)
         self.HidecomboBox.currentTextChanged.connect(self.Handel_Hide)
-        self.horizontalSlider_2.setMinimum(-1000)
+        self.horizontalSlider_2.setMinimum(-500)
         self.horizontalSlider_2.setMaximum(1000)
         self.horizontalSlider_2.setValue(0)
         self.horizontalSlider_2.setTickPosition(QSlider.TicksBelow)
-        self.horizontalSlider_2.setTickInterval(100)
-        # self.horizontalSlider_2.valueChanged.connect(self.Handel_Slider)
+        self.horizontalSlider_2.setTickInterval(50)
+        self.horizontalSlider_2.valueChanged.connect(self.Handel_Slider)
         self.PauseButton_6.clicked.connect(self.Handel_pause)
         self.PlayButton_7.clicked.connect(self.Handel_play)
         # Zoom Button Functions
@@ -73,14 +87,14 @@ class MainApp(QMainWindow , FORM_CLASS):
         self.verticalScroll.valueChanged.connect(self.scrollVertical)
         self.verticalScroll.sliderMoved.connect(self.scrollVertical)
         self.verticalScroll.sliderReleased.connect(self.scrollVertical)
+        self.ExportButton_12.clicked.connect(self.handel_export)
 
-
-    # def Handel_Slider(self):
-    #       self.speed=self.speed+self.horizontalSlider_2.value()
-    #       if(self.speed > 0):
-    #          self.timer.setInterval(self.speed)
-    #          self.timer.timeout.connect(self.update_plot_data)
-    #          self.timer.start()
+    def Handel_Slider(self):
+          self.speed=self.speed+self.horizontalSlider_2.value()
+          if(self.speed > 0):
+             self.timer.setInterval(self.speed)
+             self.timer.timeout.connect(self.update_plot_data)
+             self.timer.start()
 
     def Handel_GraphicsViews(self):
          pass
@@ -157,18 +171,22 @@ class MainApp(QMainWindow , FORM_CLASS):
         myfile = open(path[0], "r") #path refears to the chosen file after splitting   # 'r' refears to the mode, the mode here is reading 
         self.amplitude = []
         self.time = []
+        self.sigDatax=[]
+        self.sigDatay=[]
         while myfile:
             myline  = myfile.readline()
             if myline == "":  break
-            line = myline.split() 
-            self.amplitude.append(float(line [0]))
-            self.time.append(float(line [1])) 
+            self.line = myline.split() 
+            self.amplitude.append(float(self.line [0]))
+            self.time.append(float(self.line [1])) 
         myfile.close()
         if self.Signal_Flag == 1:
             self.x1 = np.array(self.time)
             self.y1 = np.array(self.amplitude)
             self.maxAmp = max(self.y1)
             self.minAmp = min(self.y1)
+            self.sigDatax.append(self.x1)
+            self.sigDatay.append(self.y1)
             if (self.maxAmp - self.minAmp) < 5:
                 self.y1*=10
                 self.maxAmp = max(self.y1)
@@ -178,6 +196,9 @@ class MainApp(QMainWindow , FORM_CLASS):
         elif self.Signal_Flag == 2:
             self.x2 = np.array(self.time)
             self.y2 = np.array(self.amplitude)
+            self.sigDatax.append(self.x2)
+            self.sigDatay.append(self.y2)
+
             if (max(self.y2) - min(self.y2)) < 5:
                 self.y2*=10
             if max(self.y2) > self.maxAmp:
@@ -189,6 +210,9 @@ class MainApp(QMainWindow , FORM_CLASS):
         elif self.Signal_Flag == 3:
             self.x3 = np.array(self.time)
             self.y3 = np.array(self.amplitude)
+            self.sigDatax.append(self.x3)
+            self.sigDatay.append(self.y3)
+
             if (max(self.y3) - min(self.y3)) < 5:
                 self.y3*=10
             if max(self.y3) > self.maxAmp:
@@ -198,9 +222,12 @@ class MainApp(QMainWindow , FORM_CLASS):
             self.Signal_Flag = 1
             self.Third_Signal_Flag = 1
         self.timer()
+        
+        
+
     def timer(self):
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(1000)                       #In control of speed :)
+        self.timer.setInterval(self.speed)                       #In control of speed :)
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
     def update_plot_data(self):
@@ -229,6 +256,56 @@ class MainApp(QMainWindow , FORM_CLASS):
                 end_count = self.counter
                 self.graphicsView.setXRange(start_count,end_count)
                 self.graphicsView.setYRange(self.minAmp, self.maxAmp)
+    
+
+    def table_creation(self, pdffilename):
+        table_header = ["Signal Name", "Mean", "Standard Deviation", "Maximum Value", "Minimum Value", 'duration']
+        table_data = [table_header]
+
+        chn = [self.HidecomboBox.itemText(0), self.HidecomboBox.itemText(1), self.HidecomboBox.itemText(2)]
+        for i in range(1):
+            amplitude = self.sigDatay[i]
+            time = self.sigDatax[i]
+            print(amplitude)
+            signal_info=["{}".format(chn[i]),round(amplitude.mean(),5),round(amplitude.std(),5),round(amplitude.max(),5),round(amplitude.min(),5),round((time.max() - time.min()),5)]
+            table_data.append(signal_info)
+
+        fig = plt.figure(constrained_layout=True)
+        fig.patch.set_visible(False)
+        fig, ax = plt.subplots()
+        ax.axis('off')
+        ax.axis('tight')
+        table = ax.table(cellText=table_data, loc='center')
+        table.scale(1, 2)
+        fig.tight_layout()
+        fileName = pdffilename + ".pdf"
+        report = PdfPages(fileName)
+        report.savefig()
+        #plt.close
+        report.close()
+
+    def print_widget(self,Plotting_Weidget ,pdf_filename):
+        ex=pg.exporters.ImageExporter(self.graphicsView.plotItem)
+        ex.export("test1.png")
+        ex = pg.exporters.ImageExporter(self.frame_3.paintEvent)
+        ex.export("test2.png")
+        doc = fitz.open(pdf_filename)  # open the PDF
+        img1 = open("test1.png", "rb").read()
+        rect1 = fitz.Rect(20, 30, 450, 110)# where to put image: use upper left corner
+        
+        for page in doc:
+            page.insert_image(rect1, stream=img1)
+
+        doc.saveIncr()
+        
+
+    def handel_export(self):
+        fn, _ = QtWidgets.QFileDialog.getSaveFileName(None, "Export PDF", None, "PDF files (.pdf);;All Files()")
+        if fn != '':
+            if QtCore.QFileInfo(fn).suffix() == "": fn += '.pdf'
+            self.table_creation(fn)
+            self.print_widget(self.graphicsView, fn)
+
 def main ():
     app = QApplication(sys.argv)
     window = MainApp ()
