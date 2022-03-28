2c29736399fae7618b51cda071b6c1728970e221
Ahmed Gamal <amgamal200111@gmail.com>
2022-03-28 19:51:46 +0200

    gui setup

--- Task 3 - Music Emphasizer/mainwindow.py
+++ Task 3 - Music Emphasizer/mainwindow.py
@@ -1,29 +1,65 @@
-# This Python file uses the following encoding: utf-8
-import os
-from pathlib import Path
+##PyQT5
+from fileinput import filename
+import string
+from subprocess import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.uic import loadUiType
+from PyQt5 import QtCore, QtGui, QtWidgets
+from pyqtgraph import PlotWidget,plot
+from PyQt5 import QtCore
+import os 
+import csv 
+import pyqtgraph as pg
+from os import path, remove 
+import numpy as np
+import pandas as pd
+import math  
+import matplotlib.pyplot as plt
+import scipy
+from scipy.fftpack import fft
+from scipy import signal 
+from tkinter import filedialog
+from tkinter import *
 import sys
 
-from PySide2.QtWidgets import QApplication, QMainWindow
-from PySide2.QtCore import QFile
-from PySide2.QtUiTools import QUiLoader
 
+FORM_CLASS,_= loadUiType(path.join(path.dirname(__file__),"qtFiles/main.ui"))
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.load_ui()
+new_sig=[]
+t = np.arange(0, 1, 0.001)
 
-    def load_ui(self):
-        loader = QUiLoader()
-        path = os.fspath(Path(__file__).resolve().parent / "qtFiles/form.ui")
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
-        ui_file.close()
+class MainApp(QMainWindow , FORM_CLASS):
+    def __init__(self, parent=None):
+        super(MainApp,self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.Handle_UI()
 
+    def Handle_UI(self):
+        self.setWindowTitle('Sampling-Theory Illustrator')
+        self.Handle_Buttons()
 
-if __name__ == "__main__":
-    app = QApplication([])
-    widget = MainWindow()
-    widget.show()
-    sys.exit(app.exec_())
+        self.Instrument1verticalSlider.setMaximum(8)
+        self.Instrument1verticalSlider.setMinimum(0)
+        self.Instrument1verticalSlider.setValue(4)
+        self.Instrument2verticalSlider.setMaximum(8)
+        self.Instrument2verticalSlider.setMinimum(0)
+        self.Instrument2verticalSlider.setValue(4)
+        self.Instrument3verticalSlider.setMaximum(8)
+        self.Instrument3verticalSlider.setMinimum(0)
+        self.Instrument3verticalSlider.setValue(4)
+
+    def Handle_Buttons(self):
+        pass
+
+        
+def main ():
+    app = QApplication(sys.argv)
+    window = MainApp ()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
