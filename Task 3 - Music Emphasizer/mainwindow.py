c3d585aad96d886bead2cd995da7561ce4932d98
Ahmed Gamal <amgamal200111@gmail.com>
2022-04-01 19:29:26 +0200

    Composing tab done

--- Task 3 - Music Emphasizer/mainwindow.py
+++ Task 3 - Music Emphasizer/mainwindow.py
@@ -23,7 +23,10 @@ from scipy import signal
 from tkinter import filedialog
 from tkinter import *
 import sys
-
+import pygame as pyg
+import sf2_loader as sf
+import playpiano as pp
+import playdrums as pd
 
 FORM_CLASS,_= loadUiType(path.join(path.dirname(__file__),"qtFiles/main.ui"))
 
@@ -36,6 +39,8 @@ class MainApp(QMainWindow , FORM_CLASS):
         QMainWindow.__init__(self)
         self.setupUi(self)
         self.Handle_UI()
+        pyg.mixer.init()
+        pyg.init()
 
     def Handle_UI(self):
         self.setWindowTitle('Sampling-Theory Illustrator')
@@ -50,11 +55,45 @@ class MainApp(QMainWindow , FORM_CLASS):
         self.Instrument3verticalSlider.setMaximum(8)
         self.Instrument3verticalSlider.setMinimum(0)
         self.Instrument3verticalSlider.setValue(4)
+        
 
     def Handle_Buttons(self):
-        pass
+        self.pianoButton1.clicked.connect(pp.playPianoNote1_1)
+        self.pianoButton1_2.clicked.connect(pp.playPianoNote1_2)
+        self.pianoButton1_3.clicked.connect(pp.playPianoNote1_3)
+        self.pianoButton1_4.clicked.connect(pp.playPianoNote1_4)
+        self.pianoButton1_5.clicked.connect(pp.playPianoNote1_5)
+        self.pianoButton1_6.clicked.connect(pp.playPianoNote1_6)
+        self.pianoButton1_7.clicked.connect(pp.playPianoNote1_7)
+        self.pianoButton2_1.clicked.connect(pp.playPianoNote2_1)
+        self.pianoButton2_2.clicked.connect(pp.playPianoNote2_2)
+        self.pianoButton2_3.clicked.connect(pp.playPianoNote2_3)
+        self.pianoButton2_4.clicked.connect(pp.playPianoNote2_4)
+        self.pianoButton2_5.clicked.connect(pp.playPianoNote2_5)
+
+        self.triangleButton.clicked.connect(playTriangle)
+
+        self.drumButtonChina1.clicked.connect(pd.playDrumChina1)
+        self.drumButtonChina2.clicked.connect(pd.playDrumChina2)
+        self.drumButtonCrash1.clicked.connect(pd.playDrumCrash1)
+        self.drumButtonCrash2.clicked.connect(pd.playDrumCrash2)
+        self.drumButtonHats1.clicked.connect(pd.playDrumHats1)
+        self.drumButtonHats2.clicked.connect(pd.playDrumHats2)
+        self.drumButtonRide.clicked.connect(pd.playDrumRide)
+        self.drumButtonSplash.clicked.connect(pd.playDrumSplash)
+        self.drumButtonStax.clicked.connect(pd.playDrumStax)
+        self.drumButtonTom1.clicked.connect(pd.playDrumTom1)
+        self.drumButtonTom2.clicked.connect(pd.playDrumTom2)
+        self.drumButtonTom3.clicked.connect(pd.playDrumTom3)
+        self.drumButtonTom4.clicked.connect(pd.playDrumTom4)
+        self.drumButtonTom5.clicked.connect(pd.playDrumTom5)
+        self.drumButtonSnare.clicked.connect(pd.playDrumSnare)
 
         
+def playTriangle(self):
+    sound = pyg.mixer.Sound('Datasests/Triangle Notes/triangle.wav')
+    sound.play()
+
 def main ():
     app = QApplication(sys.argv)
     window = MainApp ()
---
+++ Task 3 - Music Emphasizer/playdrums.py
@@ -0,0 +1,47 @@
+import pygame as pyg
+
+def playDrumChina1(self):
+    sound = pyg.mixer.Sound('Datasests\Drum Notes\china1.wav')
+    sound.play()
+def playDrumChina2(self):
+    sound = pyg.mixer.Sound('Datasests\Drum Notes\china2.wav')
+    sound.play()
+def playDrumCrash1(self):
+    sound = pyg.mixer.Sound('Datasests\Drum Notes\china1.wav')
+    sound.play()
+def playDrumCrash2(self):
+    sound = pyg.mixer.Sound('Datasests\Drum Notes\crash2.wav')
+    sound.play()
+def playDrumHats1(self):
+    sound = pyg.mixer.Sound('Datasests\Drum Notes\hat1.wav')
+    sound.play()
+def playDrumHats2(self):
+    sound = pyg.mixer.Sound('Datasests\Drum Notes\hat2.wav')
+    sound.play()
+def playDrumRide(self):
+    sound = pyg.mixer.Sound('Datasests/Drum Notes/ride.wav')
+    sound.play()
+def playDrumSplash(self):
+    sound = pyg.mixer.Sound('Datasests\Drum Notes\splash.wav')
+    sound.play()
+def playDrumTom1(self):
+    sound = pyg.mixer.Sound('Datasests/Drum Notes/tom1.wav')
+    sound.play()
+def playDrumTom2(self):
+    sound = pyg.mixer.Sound('Datasests/Drum Notes/tom2.wav')
+    sound.play()
+def playDrumTom3(self):
+    sound = pyg.mixer.Sound('Datasests/Drum Notes/tom3.wav')
+    sound.play()
+def playDrumTom4(self):
+    sound = pyg.mixer.Sound('Datasests/Drum Notes/tom4.wav')
+    sound.play()
+def playDrumTom5(self):
+    sound = pyg.mixer.Sound('Datasests/Drum Notes/tom5.wav')
+    sound.play()
+def playDrumStax(self):
+    sound = pyg.mixer.Sound('Datasests\Drum Notes\stax.wav')
+    sound.play()
+def playDrumSnare(self):
+    sound = pyg.mixer.Sound('Datasests\Drum Notes\snare.wav')
+    sound.play()
